c16aafbb673e885e498e253c9b5dbd5444b08de6
Behzad Haki <35939495+behzadhaki@users.noreply.github.com>
2022-11-18 09:24:44 +0100

    Updated batch file

--- vae_bhaki.sh
+++ vae_bhaki.sh
@@ -9,9 +9,18 @@
 #SBATCH -o %N.%J.VAE_test_loader.out
 #SBATCH -e %N.%J.VAE_test_loader.err
 
+# Necessary to access existing modules on the cluster
 source /etc/profile.d/lmod.sh
 source /etc/profile.d/zz_hpcnow-arch.sh
+
+# Load Anaconda Module, Activate Conda CLI and Activate Environment
 module load Anaconda3/2020.02
-source activate GrooveTransformer
+export PATH="/soft/easybuild/x86_64/software/Anaconda3/2020.02/bin:$PATH"
+export PATH="$HOME/.conda/envs/GrooveTransformer/bin:$PATH"
+source /soft/easybuild/x86_64/software/Anaconda3/2020.02/etc/profile.d/conda.sh
+conda activate GrooveTransformer
+
+# Run your codes here
+cd GrooveTransformer
 #wandb agent mmil_vae_g2d/SmallSweeps_MGT_VAE/bib6bpsb
 python train.py
