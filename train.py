bf46b9b5dda84bc515fb7aba1f386316429c366b
Behzad Haki <35939495+behzadhaki@users.noreply.github.com>
2022-11-26 08:47:48 +0100

    Major updates:
    1. Training on flattened tapped sequence

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import os
+
 import wandb
 import torch
 from model import GrooveTransformerEncoderVAE
@@ -31,7 +33,7 @@ parser.add_argument("--wandb_project", help="WANDB Project Name", default="Small
 parser.add_argument("--d_model_enc", help="Dimension of the encoder model", default=32)
 parser.add_argument("--d_model_dec_ratio", help="Dimension of the decoder model as a ratio of d_model_enc", default=1)
 
-parser.add_argument("--embedding_size_src", help="Dimension of the source embedding", default=27)
+parser.add_argument("--embedding_size_src", help="Dimension of the source embedding", default=3)
 parser.add_argument("--embedding_size_tgt", help="Dimension of the target embedding", default=27)
 
 parser.add_argument("--nhead_enc", help="Number of attention heads for the encoder", default=2)
@@ -53,7 +55,7 @@ parser.add_argument("--max_len_enc", help="Maximum length of the encoder", defau
 parser.add_argument("--max_len_dec", help="Maximum length of the decoder", default=32)
 
 parser.add_argument("--dropout", help="Dropout", default=0.4)
-parser.add_argument("--latent_dim", help="Dimension of the latent space at EACH TIME STEP", default=2)
+parser.add_argument("--latent_dim", help="Overall Dimension of the latent space", default=64)
 
 parser.add_argument("--hit_loss_function", help="hit_loss_function - either 'bce' or 'dice' loss",
                     default='dice', choices=['bce', 'dice'])
@@ -76,33 +78,46 @@ parser.add_argument("--is_testing", help="Use testing dataset (1% of full date) 
 # FIXME set to false if errors regarding memory
 parser.add_argument("--force_data_on_cuda", help="places all training data on cude", default=True)
 
+
+
 parser.add_argument("--optimizer", help="optimizer to use - either 'sgd' or 'adam' loss", default="sgd",
                     choices=['sgd', 'adam'])
+
 parser.add_argument("--dataset_json_dir",
                     help="Path to the folder hosting the dataset json file",
                     default="data/dataset_json_settings")
+
 parser.add_argument("--dataset_json_fname",
                     help="filename of the data (USE 4_4_Beats_gmd.jsom for only beat sections,"
                          " and 4_4_BeatsAndFills_gmd.json for beats and fill samples combined",
                     default="4_4_Beats_gmd.json")
+
+parser.add_argument("--evaluate_on_subset",
+                    help="Using test or evaluation subset for evaluating the model", default="test",
+                    choices=['test', 'evaluation'] )
+
+parser.add_argument("--calculate_hit_scores_on_train",
+                    help="Evaluates the quality of the hit models on training set",
+                    default=True)
+parser.add_argument("--calculate_hit_scores_on_test",
+                    help="Evaluates the quality of the hit models on test/evaluation set",
+                    default=True)
 
 parser.add_argument("--save_model", help="Save model", default=True)
 parser.add_argument("--save_model_dir", help="Path to save the model", default="misc/VAE")
 parser.add_argument("--save_model_frequency", help="Save model every n epochs", default=100)
 
 parser.add_argument("--piano_roll_samples", help="Generate audio samples", default=True)
-parser.add_argument("--piano_roll_frequency", help="Frequency of piano roll generation", default=10)
-
-# --------------------------------------------------------------------
-# Dummy arguments for running the script in pycharm's python console
-# --------------------------------------------------------------------
-parser.add_argument("--mode", help="IGNORE THIS PARAM", default="client")
-parser.add_argument("--port", help="IGNORE THIS PARAM", default="config.yaml")
-# --------------------------------------------------------------------
+parser.add_argument("--piano_roll_frequency", help="Frequency of piano roll generation", default=20)
+parser.add_argument("--hit_score_frequency", help="Frequency of hit score generation", default=10)
 
 args, unknown = parser.parse_known_args()
 if unknown:
     logger.warning(f"Unknown arguments: {unknown}")
+
+# Disable wandb logging in testing mode
+if args.is_testing:
+    os.environ["WANDB_MODE"] = "disabled"
 
 if args.config is not None:
     with open(args.config, "r") as f:
@@ -167,7 +182,7 @@ if __name__ == "__main__":
     config = wandb.config
     run_name = wandb_run.name
     run_id = wandb_run.id
-
+    collapse_tapped_sequence = (args.embedding_size_src == 3)
     # Load Training and Testing Datasets and Wrap them in torch.utils.data.Dataloader
     # ----------------------------------------------------------------------------------------------------------
     # only 1% of the dataset is used if we are testing the script (is_testing==True)
@@ -177,7 +192,7 @@ if __name__ == "__main__":
         subset_tag="train",
         max_len=int(args.max_len_enc),
         tapped_voice_idx=2,
-        collapse_tapped_sequence=False,
+        collapse_tapped_sequence=collapse_tapped_sequence,
         down_sampled_ratio=0.1 if args.is_testing is True else None,
         move_all_to_gpu=should_place_all_data_on_cuda)
     train_dataloader = DataLoader(training_dataset, batch_size=config.batch_size, shuffle=True)
@@ -187,7 +202,7 @@ if __name__ == "__main__":
         subset_tag="test",
         max_len=int(args.max_len_enc),
         tapped_voice_idx=2,
-        collapse_tapped_sequence=False,
+        collapse_tapped_sequence=collapse_tapped_sequence,
         down_sampled_ratio=0.1 if args.is_testing is True else None,
         move_all_to_gpu=should_place_all_data_on_cuda)
     test_dataloader = DataLoader(test_dataset, batch_size=config.batch_size, shuffle=True)
@@ -225,13 +240,17 @@ if __name__ == "__main__":
     # Iterate over epochs
     # ------------------------------------------------------------------------------------------------------------
     metrics = dict()
+    step_ = 0
     for epoch in range(config.epochs):
+        print(f"Epoch {epoch} of {config.epochs}, steps so far {step_}")
 
         # Run the training loop (trains per batch internally)
         # ------------------------------------------------------------------------------------------
         groove_transformer_vae.train()
 
-        train_log_metrics = vae_train_utils.train_loop(
+        logger.info("***************************Training...")
+
+        train_log_metrics, step_ = vae_train_utils.train_loop(
             train_dataloader=train_dataloader,
             groove_transformer_vae=groove_transformer_vae,
             optimizer=optimizer,
@@ -239,8 +258,9 @@ if __name__ == "__main__":
             velocity_loss_fn=velocity_loss_fn,
             offset_loss_fn=offset_loss_fn,
             loss_hit_penalty_multiplier=config.loss_hit_penalty_multiplier,
-            device=config.device
-        )
+            device=config.device,
+            starting_step=step_)
+
         wandb.log(train_log_metrics, commit=False)
 
         # ---------------------------------------------------------------------------------------------------
@@ -249,6 +269,8 @@ if __name__ == "__main__":
         #           rather than all at once
         # ---------------------------------------------------------------------------------------------------
         groove_transformer_vae.eval()       # DON'T FORGET TO SET THE MODEL TO EVAL MODE (check torch no grad)
+
+        logger.info("***************************Testing...")
 
         test_log_metrics = vae_train_utils.test_loop(
             test_dataloader=test_dataloader,
@@ -274,6 +296,7 @@ if __name__ == "__main__":
                     dataset_setting_json_path=f"{config.dataset_json_dir}/{config.dataset_json_fname}",
                     subset_name='test',
                     down_sampled_ratio=0.005,
+                    collapse_tapped_sequence=collapse_tapped_sequence,
                     cached_folder="eval/GrooveEvaluator/templates",
                     divide_by_genre=True,
                     need_piano_roll=True,
@@ -284,29 +307,39 @@ if __name__ == "__main__":
 
         # Get Hit Scores for the entire train and the entire test set
         # ---------------------------------------------------------------------------------------------------
-        train_set_hit_scores = vae_test_utils.get_hit_scores_for_vae_model(
-            groove_transformer_vae=groove_transformer_vae,
-            device=config.device,
-            dataset_setting_json_path=f"{config.dataset_json_dir}/{config.dataset_json_fname}",
-            subset_name='train',
-            down_sampled_ratio=0.1,
-            cached_folder="eval/GrooveEvaluator/templates",
-            divide_by_genre=False
-        )
-        wandb.log(train_set_hit_scores, commit=False)
+        if args.calculate_hit_scores_on_train:
+            if epoch % args.hit_score_frequency == 0:
+                logger.info("________Calculating Hit Scores on Train Set...")
+                train_set_hit_scores = vae_test_utils.get_hit_scores_for_vae_model(
+                    groove_transformer_vae=groove_transformer_vae,
+                    device=config.device,
+                    dataset_setting_json_path=f"{config.dataset_json_dir}/{config.dataset_json_fname}",
+                    subset_name='train',
+                    down_sampled_ratio=0.1,
+                    collapse_tapped_sequence=collapse_tapped_sequence,
+                    cached_folder="eval/GrooveEvaluator/templates",
+                    divide_by_genre=False
+                )
+                wandb.log(train_set_hit_scores, commit=False)
 
-        test_set_hit_scores = vae_test_utils.get_hit_scores_for_vae_model(
-            groove_transformer_vae=groove_transformer_vae,
-            device=config.device,
-            dataset_setting_json_path=f"{config.dataset_json_dir}/{config.dataset_json_fname}",
-            subset_name='test',
-            down_sampled_ratio=None,
-            cached_folder="eval/GrooveEvaluator/templates",
-            divide_by_genre=False
-        )
-        wandb.log(test_set_hit_scores, commit=False)
+        if args.calculate_hit_scores_on_test:
+            if epoch % args.hit_score_frequency == 0:
+                logger.info("________Calculating Hit Scores on Test Set...")
+                test_set_hit_scores = vae_test_utils.get_hit_scores_for_vae_model(
+                    groove_transformer_vae=groove_transformer_vae,
+                    device=config.device,
+                    dataset_setting_json_path=f"{config.dataset_json_dir}/{config.dataset_json_fname}",
+                    subset_name=args.evaluate_on_subset,
+                    down_sampled_ratio=None,
+                    collapse_tapped_sequence=collapse_tapped_sequence,
+                    cached_folder="eval/GrooveEvaluator/templates",
+                    divide_by_genre=False
+                )
+                wandb.log(test_set_hit_scores, commit=False)
 
-        wandb.log({"epoch": epoch}, commit=True)
+        # Commit the metrics to wandb
+        # ---------------------------------------------------------------------------------------------------
+        wandb.log({"epoch": epoch}, step=epoch)
 
         # Save the model if needed
         # ---------------------------------------------------------------------------------------------------
