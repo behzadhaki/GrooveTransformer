ffb39c3ac72e440fae24098044c04586f7080605
behzadhaki <behzad.haki@upf.edu>
2023-06-22 16:27:13 +0200

    Fully working pipeline ready for Sweeps

---
+++ helpers/Control/density_model_Loader.py
@@ -0,0 +1,46 @@
+import torch
+from model import Density1D, Density2D
+def load_density_model(model_path, density_version, params_dict=None, is_evaluating=True, device=None):
+    """ Load a GrooveTransformerEncoder model from a given path
+
+    Args:
+        model_path (str): path to the model
+        params_dict (None, dict, or json path): dictionary containing the parameters of the model
+        is_evaluating (bool): if True, the model is set to eval mode
+        device (None or torch.device): device to load the model to (if cpu, the model is loaded to cpu)
+
+    Returns:
+        model (GrooveTransformerEncoder): the loaded model
+    """
+
+    try:
+        if device is not None:
+            loaded_dict = torch.load(model_path, map_location=device)
+        else:
+            loaded_dict = torch.load(model_path)
+    except:
+        loaded_dict = torch.load(model_path, map_location=torch.device('cpu'))
+        #logger.info(f"Model was loaded to cpu!!!")
+
+    if params_dict is None:
+        if 'params' in loaded_dict:
+            params_dict = loaded_dict['params']
+        else:
+            raise Exception(f"Could not instantiate model as params_dict is not found. "
+                            f"Please provide a params_dict either as a json path or as a dictionary")
+
+    if isinstance(params_dict, str):
+        import json
+        with open(params_dict, 'r') as f:
+            params_dict = json.load(f)
+    if density_version == 2:
+        params_dict["add_params"] = False
+        model = Density2D(params_dict)
+    elif density_version == 1:
+        params_dict["add_params"] = True
+        model = Density1D(params_dict)
+    model.load_state_dict(loaded_dict["model_state_dict"])
+    if is_evaluating:
+        model.eval()
+
+    return model
--- helpers/__init__.py
+++ helpers/__init__.py
@@ -2,11 +2,12 @@ import helpers.VAE.train_utils as vae_train_utils
 import helpers.VAE.eval_utils as vae_test_utils
 import helpers.Control.eval_utils as control_eval_utils
 import helpers.Control.density_eval as density_eval
-import helpers.Control.density_1D_modelLoader as density_1D_modelLoader
-import helpers.Control.density_2D_modelLoader as density_2D_modelLoader
+# import helpers.Control.density_1D_modelLoader as density_1D_modelLoader
+# import helpers.Control.density_2D_modelLoader as density_2D_modelLoader
+import helpers.Control.density_model_Loader as density_model_Loader
 from helpers.VAE.modelLoader import load_variational_mgt_model
 
-from helpers.Control.density_2D_modelLoader import load_density_2d_model
+# from helpers.Control.density_2D_modelLoader import load_density_2d_model
 
 from helpers.BasicMonotonicGrooveTransformer.modelLoadersSamplers import load_mgt_model
 from helpers.BasicMonotonicGrooveTransformer.modelLoadersSamplers import predict_using_mgt
